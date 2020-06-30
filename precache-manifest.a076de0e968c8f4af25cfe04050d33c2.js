63e5aacafcd0004888f3bb71048a9fd21e4f302f
skooma1992 <upsonjake@gmail.com>
2020-06-30 17:43:56 -0400

    Updates

---
+++ precache-manifest.a076de0e968c8f4af25cfe04050d33c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88ba496203aa24360639c16b5f301ef",
+    "url": "/EmployeeTracker/index.html"
+  },
+  {
+    "revision": "f4f91b32e2e65a7ab696",
+    "url": "/EmployeeTracker/static/css/main.1376bb6b.chunk.css"
+  },
+  {
+    "revision": "7494448822be56b1c687",
+    "url": "/EmployeeTracker/static/js/2.8a407463.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/EmployeeTracker/static/js/2.8a407463.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4f91b32e2e65a7ab696",
+    "url": "/EmployeeTracker/static/js/main.9480d070.chunk.js"
+  },
+  {
+    "revision": "835d521a4fa7a0ecb055",
+    "url": "/EmployeeTracker/static/js/runtime-main.0bff5947.js"
+  }
+]);
